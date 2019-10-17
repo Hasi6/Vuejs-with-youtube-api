821f7ee3a10bd4a3ebd80f048f4d497c7dd12d28
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-10-17 21:11:47 +0530

    create project structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SearchBar from './components/SearchBar/SearchBar'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SearchBar
   }
 }
 </script>
 
+<template>
+  <SearchBar msg="Hasi"/>
+</template>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <input type="text">
+</template>
+
+<script>
+    export default{
+        name: 'Search Bar',
+        props: {
+            msg: String
+        }
+    }
+</script>
