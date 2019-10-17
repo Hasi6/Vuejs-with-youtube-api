168b7a5e1649331854d40edf021a9736c739ad76
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-10-17 22:54:58 +0530

    get youtube api key

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
-<script>
-import SearchBar from './components/SearchBar/SearchBar'
-export default {
-  name: 'app',
-  components: {
-    SearchBar
-  }
-}
-</script>
-
 <template>
-  <SearchBar msg="Hasi"/>
+  <div>
+    <SearchBar @termChange="onTermChanged"></SearchBar>
+  </div>
 </template>
 
-<style>
+<script>
+import SearchBar from "./components/SearchBar/SearchBar";
+import axios from "axios";
+const API_KEY = "AIzaSyDwSsXQ4V8GBH0PGfuAw9muk-_Y8d5hsjY";
+const setKeyWord = async word => {
+  try {
+    
+    const res = await axios.get(`https://www.googleapis.com/youtube/v3/search/`, {
+      params: {
+        key: API_KEY,
+        type: "video",
+        part: "snippet",
+        q: word
+      }
+    });
+    console.log(res)
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
+export default {
+  name: "app",
+  components: {
+    SearchBar
+  },
+  methods: {
+    onTermChanged: word => setKeyWord(word)
+  }
+};
+</script>
+<style>
 </style>
--- src/components/SearchBar/SearchBar.vue
+++ src/components/SearchBar/SearchBar.vue
@@ -1,12 +1,12 @@
 <template>
-    <input type="text">
+  <div>
+    <input @input="$emit('termChange', $event.target.value)" />
+  </div>
 </template>
 
 <script>
-    export default{
-        name: 'Search Bar',
-        props: {
-            msg: String
-        }
-    }
+
+export default {
+  name: "Search_Bar"
+};
 </script>
